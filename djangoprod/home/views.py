0d5ef40c295d3a8a36bb8852d37bd5322a266773
Robert Neuzil <robertneuzil@gmail.com>
2018-09-24 15:35:30 -0400

    template looping

--- djangoprod/home/views.py
+++ djangoprod/home/views.py
@@ -1,11 +1,43 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+posts = [
+
+{
+	"author": "Robert N.",
+	"title": "Hello",
+	"date": "09/24/2018"
+
+},
+
+{
+	
+	"author": "Jay Shoe",
+	"title": "I invented shoes",
+	"date": "04/11/2008"
+
+
+},
+
+{
+	
+	"author": "Dr. X",
+	"title": "Jet Fuel Doesn't Melt Steel Beams",
+	"date": "09/12/2002"
+
+
+},
+
+
+]
 
 
 def index(request):
-	return render(request, 'home/index.html')
+	context = {
+	'posts' : posts
+
+	}
+	return render(request, 'home/index.html', context)
 
 def about(request):
 	return render(request, 'home/about.html')
-# Create your views here.
+
