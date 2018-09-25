560873e05f8b244b4c21a0d5fbb30a5087641ef8
Robert Neuzil <robertneuzil@gmail.com>
2018-09-25 00:20:45 -0400

    model objects

--- djangoprod/home/models.py
+++ djangoprod/home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+class Blog_Post(models.Model):
+
+	author = models.CharField(max_length=100)
+	title = models.CharField(max_length=100)
+	date = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.title
+
+
+
 # Create your models here.
--- djangoprod/home/views.py
+++ djangoprod/home/views.py
@@ -1,41 +1,17 @@
 from django.shortcuts import render
-
-posts = [
-
-{
-	"author": "Robert N.",
-	"title": "Hello",
-	"date": "09/24/2018"
-
-},
-
-{
-	
-	"author": "Jay Shoe",
-	"title": "I invented shoes",
-	"date": "04/11/2008"
+from .models import Blog_Post
 
 
-},
-
-{
-	
-	"author": "Dr. X",
-	"title": "Jet Fuel Doesn't Melt Steel Beams",
-	"date": "09/12/2002"
-
-
-},
-
-
-]
 
 
 def index(request):
-	context = {
-	'posts' : posts
 
-	}
+	
+	posts = Blog_Post.objects.order_by('id')
+
+
+	
+	context = {'posts': posts}
 	return render(request, 'home/index.html', context)
 
 def about(request):
