799bff64790c67c4f64c368d95dd16ae33f97c35
Robert Neuzil <robertneuzil@gmail.com>
2018-09-26 09:20:39 -0400

    forms

--- djangoprod/home/forms.py
+++ djangoprod/home/forms.py
@@ -4,19 +4,15 @@ class EntryForm(forms.Form):
 	author = forms.CharField(max_length=40,
 		widget=forms.TextInput(
 			attrs={
-			'class': 'form-group floating-label-form-group controls',
-			'class': 'form-control',
-			'placeholder': 'Author',
-			'id': 'Author',
-			'required data-validation-required-message': 'Please enter your name.',}))
+			
+			'class': '',
+}))
 	date = forms.CharField(max_length=40,
 		widget = forms.TextInput(
 			attrs={
-			'class': 'form-group floating-label-form-group controls',
-			'class': 'form-control',
-			'placeholder': 'Date',
-			'id': 'Date',
-			'required data-validation-required-message': 'Please enter a date address.',
+
+			'class': 'form-control'
+
 
 
 
@@ -24,13 +20,10 @@ class EntryForm(forms.Form):
 	message = forms.CharField(max_length=200,
 		widget = forms.TextInput(
 			attrs={
-			'class': 'form-group floating-label-form-group controls',
-			'class': 'form-control',
-			'placeholder': 'Message',
-			'id': 'message',
-			'required data-validation-required-message': 'Please enter a Message',
-			'rows': '50',
-			'col': '50'
+
+			'class': ''
+
+
 
 
 
